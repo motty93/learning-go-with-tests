73af50d10738c18a79afbd78c5407b301489c692
motty93 <rdwbocungelt5@gmail.com>
2024-12-06 13:56:58 +0900

    feat: Add multi-language support to Hello function

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello() string {
-	return "Hello, World!"
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return fmt.Sprintf("%s%s!", greetingPrefix(language), name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("World", ""))
 }
